2584030285aef2406cba8b3bc35264983e18c092
awololaeinola-lang <awololaeinola@gmail.com>
2026-02-02 01:01:52 -1000

    Fix module system for Render

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const todoRoutes = require("./routes/todoRoutes.js");
+const dotenv = require("dotenv");
 
-require("dotenv").config();
-
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .catch(err => console.error("MongoDB error:", err));
 
-app.use("/api/todos", todoRoutes);
+app.get("/", (req, res) => {
+  res.send("API running");
+});
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
